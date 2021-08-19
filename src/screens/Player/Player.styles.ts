cfdb1ae1a129236b2a4a0bbe9fb7513a30e6a3b7
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-19 19:16:55 +0300

    add player screen

---
+++ src/screens/Player/Player.styles.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from "react-native";
+
+export const PlayerStyles = StyleSheet.create({
+  Player: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#493673',
+  }
+})
