e015846e60904dc59e3d697ef75c27607513e2d5
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-20 21:32:13 +0300

    add Header

---
+++ src/screens/Player/Header/Header.styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native";
+
+export const HeaderStyles = StyleSheet.create({
+  container: {
+    flex: 0.5,
+    flexGrow: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: 'rgba(255, 255, 255, 0.7)',
+    fontSize: 11,
+  },
+  trackName: {
+    color: 'white',
+    fontSize: 14,
+  },
+});
