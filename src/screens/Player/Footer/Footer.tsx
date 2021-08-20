e289d9cd6c75a53980d5fbbaf7c40f36d604f273
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-20 21:32:20 +0300

    add Footer

---
+++ src/screens/Player/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from "react-native";
+import {FooterStyles as styles} from "./Footer.styles";
+
+const Footer: React.FC = () => {
+
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
+
+export default Footer;
