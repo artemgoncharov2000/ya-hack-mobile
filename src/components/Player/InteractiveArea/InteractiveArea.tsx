5f7701a732070b4e3f4f3d5d59499696051a881c
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-21 12:29:28 +0300

    update InteractiveArea.tsx

--- src/components/Player/InteractiveArea/InteractiveArea.tsx
+++ src/components/Player/InteractiveArea/InteractiveArea.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View} from "react-native";
 import {InteractiveAreaStyles as styles} from "./InteractiveAreaStyles";
 import Item from "./Item/Item";
+import Poll from "./Poll/Poll";
 
 
 const InteractiveArea: React.FC = () => {
@@ -9,7 +10,7 @@ const InteractiveArea: React.FC = () => {
   const testImageUrl = '../../images/test-image.jpeg';
   return (
     <View style={styles.container}>
-      <Item backGroundImageUrl={testImageUrl}/>
+      <Poll/>
     </View>
   );
 }
