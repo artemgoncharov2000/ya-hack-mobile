d6c6e26bb038625dfd658274b6b08d7758ab8d62
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-21 17:58:05 +0300

    add time to Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text} from "react-native";
 import MultiSlider from "@ptomasroos/react-native-multi-slider";
 import {SliderStyles as styles} from "./Slider.styles";
+import moment from "moment";
 
 type PropsT = {
   currentPos: number,
@@ -13,13 +14,17 @@ const Slider: React.FC<PropsT> = ({
   duration,
 }) => {
 
+  const getTime = (millis: number) => {
+    const duration = moment.duration(millis);
+    const hours = Math.trunc(duration.asHours());
+    const minutes = Math.trunc(duration.asMinutes());
+    const seconds = Math.trunc(duration.asSeconds());
+    return `${hours}:${minutes}:${seconds}`;
+  }
 
   return (
     <View>
       <MultiSlider
-        // customMarker={(e)=> {
-        //   return (<Marker pressed={e.pressed}/>);
-        // }}
         pressedMarkerStyle={styles.pressedMarker}
         markerStyle={styles.marker}
         selectedStyle={styles.selected}
@@ -27,8 +32,8 @@ const Slider: React.FC<PropsT> = ({
         containerStyle={styles.sliderContainer}
       />
       <View style={styles.timeStatusContainer}>
-        <Text style={styles.timeText}>{currentPos}</Text>
-        <Text style={styles.timeText}>{duration}</Text>
+        <Text style={styles.timeText}>{getTime(currentPos)}</Text>
+        <Text style={styles.timeText}>{getTime(duration)}</Text>
       </View>
     </View>
 
