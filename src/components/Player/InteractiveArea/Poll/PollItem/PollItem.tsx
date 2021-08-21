8cc0c560ff4aa0590640d936bc29e3496a7ffdaf
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-21 22:34:24 +0300

    support radioButton

--- src/components/Player/InteractiveArea/Poll/PollItem/PollItem.tsx
+++ src/components/Player/InteractiveArea/Poll/PollItem/PollItem.tsx
@@ -3,6 +3,9 @@ import {View, Text, TouchableOpacity} from "react-native";
 import {PollItemStyles as styles} from "./PollItem.styles";
 import CheckBoxIcon from '../../../../../icons/check-box-icon.svg';
 import CheckBoxSelectedIcon from '../../.././../../icons/check-box-selected-icon.svg';
+import RadioButtonIcon from '../../../../../icons/radio-button-icon.svg';
+import RadioButtonSelectedIcon from '../../../../../icons/radio-button-selected-icon.svg';
+
 
 type CheckBoxPropsT = {
   selected: boolean;
@@ -25,15 +28,35 @@ const CheckBox: React.FC<CheckBoxPropsT> = ({
   );
 };
 
-const RadioButton: React.FC = () => {
+type RadioButtonPropsT = {
+  selected: boolean;
+};
+
+const RadioButton: React.FC<RadioButtonPropsT> = ({
+  selected,
+}) => {
   return (
     <TouchableOpacity>
-
+      {
+        selected
+          ?
+          <RadioButtonIcon/>
+          :
+          <RadioButtonSelectedIcon/>
+      }
     </TouchableOpacity>
   );
 }
 
-const PollItem: React.FC = () => {
+type PollItemPropsT = {
+  text: string;
+  multipleAnswers: boolean;
+}
+
+const PollItem: React.FC<PollItemPropsT> = ({
+  text,
+  multipleAnswers,
+}) => {
 
   const [selected, setSelected] = useState(false);
 
@@ -44,10 +67,16 @@ const PollItem: React.FC = () => {
   return (
     <TouchableOpacity style={styles.container} onPress={handlePress}>
       <View style={styles.checkBoxContainer}>
-        <CheckBox selected={selected}/>
+        {
+          multipleAnswers
+            ?
+            <CheckBox selected={selected}/>
+            :
+            <RadioButton selected={selected}/>
+        }
       </View>
       <View>
-        <Text style={styles.text}>PollItemText</Text>
+        <Text style={styles.text}>{text}</Text>
       </View>
     </TouchableOpacity>
   )
