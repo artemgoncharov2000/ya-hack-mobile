ef79568780d6084ea9e93109e87b0d5b928f3674
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 11:44:42 +0300

    update InteractiveArea

--- src/components/Player/InteractiveArea/InteractiveArea.tsx
+++ src/components/Player/InteractiveArea/InteractiveArea.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import {View} from "react-native";
 import {InteractiveAreaStyles as styles} from "./InteractiveAreaStyles";
-import Item from "./Item/Item";
+import Cover from "./Cover/Cover";
 import Poll from "./Poll/Poll";
+import ImageButton from "./ImageButton/ImageButton";
+import TextArea from "./TextArea/TextArea";
 
 
 const InteractiveArea: React.FC = () => {
 
+  const PollMock = {
+    question: "Какая удаленка лучше всего подходит для удаления?",
+    options: [
+      'Рязань',
+      'На Бали',
+      'Старбакс на Савеловской',
+      'В рязанской области',
+    ],
+  }
+
+  const text = "Термин — слово или словосочетание, являющееся названием определённого понятия какой-н. специальной области науки, техники, искусства";
+
   const testImageUrl = '../../images/test-image.jpeg';
   return (
     <View style={styles.container}>
-      <Poll/>
+      <TextArea text={text}/>
     </View>
   );
 }
