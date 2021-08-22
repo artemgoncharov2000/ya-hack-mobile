432dbc850027ded9c13c9180a9e4bf01a065882d
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 20:33:04 +0300

    add getItemsByKey selector

--- src/modules/episode/selectors.ts
+++ src/modules/episode/selectors.ts
@@ -1,4 +1,6 @@
 import {EpisodeStateT} from "./model";
 import {RootState} from "../reducer";
+import keyBy from 'lodash/keyBy';
 
 export const selectEpisode = (state: RootState): EpisodeStateT => state.episode;
+export const getItemsByKey = (state: RootState) => keyBy(state.episode.items, 'timeStart');
