c87f1d56dc0314eb84e6ee2ef6c6a9a03b70382d
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 17:22:13 +0300

    add episode reducer

---
+++ src/modules/episode/reducer.ts
@@ -0,0 +1,35 @@
+import {types as t} from './actions';
+import {EpisodeStateT, ItemEnum} from "./model";
+import {ActionT} from "../model";
+
+const initialState: EpisodeStateT = {
+  podcastId: '',
+  guid: '',
+  episodeNumber: 0,
+  title: '',
+  duration: 0,
+  url: '',
+  description: '',
+  published: false,
+  items: null,
+}
+
+export default (state = initialState, action: ActionT): EpisodeStateT => {
+  switch (action.type) {
+    case t.LOAD_EPISODE_SUCCESS: {
+      return {
+        ...action.episode
+      }
+    }
+    case t.LOAD_EPISODE_FAILURE: {
+      console.log('Failed to load episode');
+      console.log('Reason: ', action.error);
+      return state;
+    }
+
+    default: {
+      return state;
+    }
+
+  }
+}
