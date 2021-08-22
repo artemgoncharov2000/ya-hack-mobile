687a172c5784a7279160a6739c8d1283405d8979
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 17:24:33 +0300

    add store

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Player from "./src/components/Player/Player";
+import rootSaga from "./src/modules/sagas";
+import rootReducer, {RootState} from "./src/modules/reducer";
+import {Provider} from "react-redux";
+import createSagaMiddleware from 'redux-saga';
+import {applyMiddleware, compose, createStore} from "redux";
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware),
+)
+
+sagaMiddleware.run(rootSaga);
+
 
 export default function App() {
   return (
-    <Player/>
+    <Provider store={store}>
+      <Player/>
+    </Provider>
   );
 }
