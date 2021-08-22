3b98edde90a079b65558ee9af8ba227666531b89
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 12:09:45 +0300

    add Episode model

---
+++ src/modules/episode/model.ts
@@ -0,0 +1,32 @@
+export enum ItemEnum {
+  POLL = 'pol',
+  IMAGE_BUTTON = 'imagebutton',
+  TEXT = 'text',
+  FORM = 'form',
+}
+
+export type EpisodeItemT = {
+  id: string,
+  type: ItemEnum,
+  timeStart: number,
+  timeEnd: number,
+  question?: string,
+  options?: string,
+  multipleAnswer?: boolean,
+  text?: string,
+  buttonUrl?: string,
+  buttonText?: string,
+  imageUrl?: string,
+};
+
+export type EpisodeT = {
+  podcastId: string,
+  guid: string,
+  episodeNumber: number,
+  title: string,
+  duration: number,
+  url: string,
+  description: string,
+  published: boolean,
+  items: Array<EpisodeItemT> | null,
+};
