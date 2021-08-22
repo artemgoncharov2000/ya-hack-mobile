a4c99493930c541e7b076ec0d4cad9fc6ed6779b
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 20:32:43 +0300

    update RewindButton

--- src/components/RewindButton/RewindButton.tsx
+++ src/components/RewindButton/RewindButton.tsx
@@ -1,34 +1,26 @@
 import React, {useState} from "react";
-import { View } from "react-native";
+import {View} from "react-native";
 import {TouchableOpacity} from "react-native";
 import BackwardIcon from '../../icons/backward-button-svg.svg';
 import ForwardIcon from '../../icons/forward-button-icon.svg';
 
 type PropsT = {
   isForward: boolean;
+  onPress: () => void;
 };
 
-const RewindButton: React.FC<PropsT> = ({isForward}) => {
-
-  // const [isPlaying, setIsPlaying] = useState(false);
-  //
-  // const handleTouch = () => {
-  //   setIsPlaying(!isPlaying);
-  // }
+const RewindButton: React.FC<PropsT> = ({isForward, onPress}) => {
 
   return (
-    <View>
-      <TouchableOpacity>
-        {
-          isForward
-            ?
-            <ForwardIcon />
-            :
-            <BackwardIcon />
-        }
-      </TouchableOpacity>
-    </View>
-
+    <TouchableOpacity onPress={onPress}>
+      {
+        isForward
+          ?
+          <ForwardIcon/>
+          :
+          <BackwardIcon/>
+      }
+    </TouchableOpacity>
   )
 }
 
