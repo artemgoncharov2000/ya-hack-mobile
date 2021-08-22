39e863c57bd0dd7ef54e8f26d8240cbb28eb7c93
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 20:31:25 +0300

    add currentItem action

--- src/modules/model.ts
+++ src/modules/model.ts
@@ -1,4 +1,7 @@
 import {ActionT as EpisodeAction} from "./episode/actions";
+import {ActionT as CurrentItemAction} from "./current-item/actions";
 
 export type ActionT =
   | EpisodeAction
+  | CurrentItemAction
+
