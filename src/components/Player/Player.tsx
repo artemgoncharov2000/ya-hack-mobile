323bb92d0c1a941703f481b8bb932b5a16c6f2a2
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 17:25:03 +0300

    update Player.tsx

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, ScrollView, SliderComponent, View} from "react-native";
 import MultiSlider from "@ptomasroos/react-native-multi-slider";
 import {PlayerStyles} from "./Player.styles";
@@ -7,18 +7,28 @@ import InteractiveArea from "./InteractiveArea/InteractiveArea";
 import SongInfo from "./SongInfo/SongInfo";
 import PlayControls from "./PlayControls/PlayControls";
 import Footer from "./Footer/Footer";
+import {useDispatch, useSelector} from "react-redux";
+import {loadEpisode} from "../../modules/episode/actions";
+import {selectEpisode} from "../../modules/episode/selectors";
 
 const Player: React.FC = () => {
+  const dispatch = useDispatch()
+  const episode = useSelector(selectEpisode);
+
+  useEffect(() => {
+    const guid = '4ceec985-08bb-c5b3-ca67-1b78d33a91df';
+    dispatch(loadEpisode(guid));
+  }, [])
 
   return (
     <SafeAreaView style={PlayerStyles.Player}>
-      <Header title={"Sebrant Chatting"}/>
+      <Header title={episode.title}/>
       <InteractiveArea/>
       <SongInfo
-        title={'Очередной подкаст Себранта'}
-        subtitle={'Sebrant Chatting'}
+        title={episode.description}
+        subtitle={episode.title}
       />
-      <PlayControls/>
+      <PlayControls trackUrl={episode.url}/>
       <Footer/>
     </SafeAreaView>
   )
