2b6a2c3600afc35bbf29ca52459fab5299a20848
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 20:33:50 +0300

    update rootReducer

--- src/modules/reducer.ts
+++ src/modules/reducer.ts
@@ -1,13 +1,17 @@
 import episode from './episode/reducer';
+import currentItem from './current-item/reducer';
 import {EpisodeStateT} from "./episode/model";
 import {combineReducers} from "redux";
+import {CurrentItemStateT} from "./current-item/model";
 
 const rootReducer = combineReducers({
   episode,
+  currentItem,
 });
 
 export default rootReducer;
 
 export interface RootState {
   episode: EpisodeStateT;
+  currentItem: CurrentItemStateT,
 }
