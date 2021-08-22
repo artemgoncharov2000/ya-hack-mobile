055b075efd127c2b8fa39b8613051e6aa1d74e36
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-22 20:36:20 +0300

    remove mock data from interactive area

--- src/components/Player/InteractiveArea/InteractiveArea.tsx
+++ src/components/Player/InteractiveArea/InteractiveArea.tsx
@@ -5,26 +5,18 @@ import Cover from "./Cover/Cover";
 import Poll from "./Poll/Poll";
 import ImageButton from "./ImageButton/ImageButton";
 import TextArea from "./TextArea/TextArea";
+import {useSelector} from "react-redux";
+import {getItemsByKey} from "../../../modules/episode/selectors";
 
 
 const InteractiveArea: React.FC = () => {
 
-  const PollMock = {
-    question: "Какая удаленка лучше всего подходит для удаления?",
-    options: [
-      'Рязань',
-      'На Бали',
-      'Старбакс на Савеловской',
-      'В рязанской области',
-    ],
-  }
 
-  const text = "Термин — слово или словосочетание, являющееся названием определённого понятия какой-н. специальной области науки, техники, искусства";
 
   const testImageUrl = '../../images/test-image.jpeg';
   return (
     <View style={styles.container}>
-      <TextArea text={text}/>
+      <TextArea text={''}/>
     </View>
   );
 }
