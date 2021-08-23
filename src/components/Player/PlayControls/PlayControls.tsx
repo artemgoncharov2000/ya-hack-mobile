e7cc2d6c26880d1b6454cb71d97acc676cfc958c
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-23 20:27:26 +0300

    update PlayControls.tsx

--- src/components/Player/PlayControls/PlayControls.tsx
+++ src/components/Player/PlayControls/PlayControls.tsx
@@ -3,14 +3,18 @@ import React, {useEffect, useState} from 'react';
 import {View, Text, Button, ActivityIndicator} from "react-native";
 import {ControlsStyles as styles} from "./Controls.styles";
 import PlayButton from "../../PlayButton/PlayButton";
+import isNil from 'lodash/isNil';
 // import Slider from "../../Slider/Slider";
 import RewindButton from "../../RewindButton/RewindButton";
 import {Audio, AVPlaybackStatus} from 'expo-av';
 import { Sound } from 'expo-av/build/Audio/Sound';
 import Slider from '@react-native-community/slider';
 import moment from "moment";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {getItemsByKey} from "../../../modules/episode/selectors";
+import {setCurrentItem} from "../../../modules/current-item/actions";
+import PlayerSlider from "../../Slider/Slider";
+import {selectCurrentItem} from "../../../modules/current-item/selectors";
 
 const uri =  "file:///Users/artemgoncharov/Library/Developer/CoreSimulator/Devices/586D35AB-0332-42CE-B75E-287E6B5AC19E/data/Containers/Data/Application/99DD4F57-C1AE-4196-B18C-EB65376CCD03/Library/Caches/ExponentExperienceData/%2540artemgoncharov2000%252FYaHackMobile/ExponentAsset-a8498bd8aa4b8070ad0a07977277cafc.mp3"
 type PropsT = {
@@ -18,24 +22,50 @@ type PropsT = {
 }
 
 const PlayControls: React.FC<PropsT> = ({trackUrl}) => {
+  const currentItem = useSelector(selectCurrentItem);
+  const dispatch = useDispatch();
   const itemsByKey = useSelector(getItemsByKey);
-  console.log('itemByKey', itemsByKey);
   const [loaded, setLoaded] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [playing, setPlaying] = React.useState(false);
   const [duration, setDuration] = React.useState(0);
   const [value, setValue] = React.useState(0);
+  const [currentMillis, setCurrentMillis] = React.useState(0);
   const sound = React.useRef(new Audio.Sound());
+
+
+
+  const updateItems = (seconds: number) => {
+    const item = itemsByKey[seconds];
+
+    if (isNil(item)) {
+      return;
+    }
+
+    dispatch(setCurrentItem(item));
+  };
+
+  const updateCurrentItem = (seconds: number) => {
+    if (isNil(currentItem)) {
+      return;
+    }
+
+    if (currentItem.timeEnd === seconds) {
+      dispatch(setCurrentItem(null));
+    }
+  }
 
   const updateStatus = async (data) => {
     try {
       if (data.didJustFinish) {
-        console.log('update status 1');
         await resetPlayer();
       } else if (data.positionMillis) {
         const duration = moment.duration(data.positionMillis);
         const seconds = Math.trunc(duration.asSeconds());
-        console.log('positionsSec: ', seconds);
+        console.log('currentSeconds:', seconds);
+        setCurrentMillis(data.positionMillis);
+        updateItems(seconds);
+        updateCurrentItem(seconds);
         if (data.durationMillis) {
           setValue((data.positionMillis / data.durationMillis) * 100);
         }
@@ -53,6 +83,7 @@ const PlayControls: React.FC<PropsT> = ({trackUrl}) => {
         setPlaying(false);
         await sound.current.setPositionAsync(0);
         await sound.current.stopAsync();
+
       }
     } catch (error) {
       console.log('Error');
@@ -84,6 +115,7 @@ const PlayControls: React.FC<PropsT> = ({trackUrl}) => {
           setPlaying(false);
         }
       }
+
     } catch (error) {
       setPlaying(true);
     }
@@ -95,6 +127,7 @@ const PlayControls: React.FC<PropsT> = ({trackUrl}) => {
       if (checkLoading.isLoaded) {
         const result = (currentPos / 100) * duration;
         await sound.current.setPositionAsync(Math.round(result));
+        setCurrentMillis(Math.round(result));
       }
     } catch (error) {
       console.log('Error');
@@ -143,24 +176,23 @@ const PlayControls: React.FC<PropsT> = ({trackUrl}) => {
 
   useEffect(() => {
     loadAudio();
-  }, []);
+    console.log('loaded');
+  }, [loaded, setLoaded]);
 
     return (
       <View style={styles.container}>
         {/*<Slider currentPos={Value} duration={Duration}/>*/}
-        <Slider
-          style={{ width: 365 }}
-          minimumValue={0}
-          maximumValue={100}
+
+        <PlayerSlider
           value={value}
-          onValueChange={(value) => {console.log(value)}}
+          currentMillis={currentMillis}
+          duration={duration}
           onSlidingComplete={(currentPos) => seekUpdate(currentPos)}
-          minimumTrackTintColor={'dodgerblue'}
         />
         <View style={styles.buttonsContainer}>
           <RewindButton isForward={false} onPress={handleMoveBackward}/>
           {loading ? (
-            <ActivityIndicator size={"small"} color={"red"}/>
+            <ActivityIndicator size={"small"} color={"white"}/>
           )
           : loaded && (
               <PlayButton playing={playing} onPress={playing ? pauseAudio : playAudio}/>
