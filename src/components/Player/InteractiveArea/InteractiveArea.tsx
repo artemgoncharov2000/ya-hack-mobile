34342ba14c05ad1f4c5115dac707e3569a311e6f
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-23 20:28:00 +0300

    update InteractiveArea

--- src/components/Player/InteractiveArea/InteractiveArea.tsx
+++ src/components/Player/InteractiveArea/InteractiveArea.tsx
@@ -2,21 +2,54 @@ import React from 'react';
 import {View} from "react-native";
 import {InteractiveAreaStyles as styles} from "./InteractiveAreaStyles";
 import Cover from "./Cover/Cover";
-import Poll from "./Poll/Poll";
 import ImageButton from "./ImageButton/ImageButton";
 import TextArea from "./TextArea/TextArea";
 import {useSelector} from "react-redux";
-import {getItemsByKey} from "../../../modules/episode/selectors";
+import {selectCurrentItem} from "../../../modules/current-item/selectors";
+import {EpisodeItemT} from "../../../modules/episode/model";
+import {ItemEnum} from "../../../modules/current-item/model";
+import Poll from "./Poll/Poll";
+
+type PropsT = {
+  defaultImageUrl: string;
+}
+const InteractiveArea: React.FC<PropsT> = ({
+  defaultImageUrl,
+}) => {
+
+  const currentItem = useSelector(selectCurrentItem);
+
+  const getItemComponent = (item: EpisodeItemT) => {
+      switch (item.type) {
+        case ItemEnum.IMAGE_BUTTON:
+          return (
+            <ImageButton imageUrl={item.imageUrl} buttonText={item.buttonText} buttonUrl={item.buttonUrl}/>
+          )
+        case ItemEnum.TEXT:
+          return (
+            <TextArea text={item.text ?? ''}/>
+          )
+        case ItemEnum.FORM:
+          return (
+            <Poll question={item.question ?? ''} />
+          )
+        case ItemEnum.POLL:
+          return (
+            <Poll question={item.question ?? ''} options={item.options} hasMultipleAnswers={item.multipleOptions}/>
+          )
+      }
+  }
 
 
-const InteractiveArea: React.FC = () => {
-
-
-
-  const testImageUrl = '../../images/test-image.jpeg';
   return (
     <View style={styles.container}>
-      <TextArea text={''}/>
+      {
+        currentItem
+        ?
+          getItemComponent(currentItem)
+        :
+          <Cover backgroundImageUrl={defaultImageUrl}/>
+      }
     </View>
   );
 }
