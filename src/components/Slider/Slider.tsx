b8338d410bb0e4a997aef35d06af9511137a0447
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-23 20:27:11 +0300

    update Slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,39 +1,55 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from "react-native";
 import MultiSlider from "@ptomasroos/react-native-multi-slider";
 import {SliderStyles as styles} from "./Slider.styles";
 import moment from "moment";
+import Slider from "@react-native-community/slider";
 
 type PropsT = {
-  currentPos: number,
+  value: number,
   duration: number,
+  onSlidingComplete: (value: number) => {},
+  currentMillis: number,
 }
 
-const Slider: React.FC<PropsT> = ({
-  currentPos,
+const PlayerSlider: React.FC<PropsT> = ({
+  value,
   duration,
+  onSlidingComplete,
+  currentMillis,
 }) => {
 
   const getTime = (millis: number) => {
+    // console.log('millis', millis);
     const duration = moment.duration(millis);
-    const hours = Math.trunc(duration.asHours());
-    const minutes = Math.trunc(duration.asMinutes());
+
     const seconds = Math.trunc(duration.asSeconds());
-    return `${hours}:${minutes}:${seconds}`;
+    let date = new Date(seconds * 1000);
+    // console.log('millis', seconds);
+    return date.toISOString().substr(11, 8);
   }
 
   return (
     <View>
-      <MultiSlider
-        pressedMarkerStyle={styles.pressedMarker}
-        markerStyle={styles.marker}
-        selectedStyle={styles.selected}
-        sliderLength={365}
-        containerStyle={styles.sliderContainer}
+      {/*<MultiSlider*/}
+      {/*  pressedMarkerStyle={styles.pressedMarker}*/}
+      {/*  markerStyle={styles.marker}*/}
+      {/*  selectedStyle={styles.selected}*/}
+      {/*  sliderLength={365}*/}
+      {/*  containerStyle={styles.sliderContainer}*/}
 
+      {/*/>*/}
+      <Slider
+        style={{ width: 365 }}
+        minimumValue={0}
+        maximumValue={100}
+        value={value}
+        // onValueChange={() => {console.log(value)}}
+        onSlidingComplete={onSlidingComplete}
+        minimumTrackTintColor={'white'}
       />
       <View style={styles.timeStatusContainer}>
-        <Text style={styles.timeText}>{getTime(currentPos)}</Text>
+        <Text style={styles.timeText}>{getTime(currentMillis)}</Text>
         <Text style={styles.timeText}>{getTime(duration)}</Text>
       </View>
     </View>
@@ -41,5 +57,5 @@ const Slider: React.FC<PropsT> = ({
   );
 };
 
-export default Slider;
+export default PlayerSlider;
 
