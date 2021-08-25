e0da0d0bc381afeed5374613525da335bf2e40bd
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-08-25 18:28:52 +0300

    update App

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Player from "./src/components/Player/Player";
+
 import rootSaga from "./src/modules/sagas";
 import rootReducer, {RootState} from "./src/modules/reducer";
-import {Provider} from "react-redux";
+import {Provider, useSelector} from "react-redux";
 import createSagaMiddleware from 'redux-saga';
 import {applyMiddleware, compose, createStore} from "redux";
-import Podcast from "./src/components/Podcast/Podcast";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Navigation from "./src/navigation/Navigation";
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,25 +17,13 @@ export const store = createStore(
 window.store = store;
 sagaMiddleware.run(rootSaga);
 
-const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={"EpisodesList"}
-        >
-          <Stack.Screen
-            name={"EpisodesList"}
-            component={Podcast}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen name={"Player"} component={Player}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation/>
     </Provider>
   );
 }
